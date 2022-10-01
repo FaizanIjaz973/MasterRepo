28885fac6f3a7f518376c8a77df9026346d2c2ed
FaizanIjaz973 <faizanijaz@gmail.com>
2022-10-01 02:54:05 +0200

    Added log statement in the Main Activity

--- app/src/main/java/com/example/masterrepo/MainActivity.kt
+++ app/src/main/java/com/example/masterrepo/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.masterrepo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+    private val logTag = "Main Activity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(logTag, "In the onCreate of the Main Activity")
     }
 }
